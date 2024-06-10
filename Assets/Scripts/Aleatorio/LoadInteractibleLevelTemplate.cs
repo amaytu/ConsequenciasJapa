6eba715f993ad975c68a42f0659e977ddbc1f13f
GabrielUtyama <102330290+amaytu@users.noreply.github.com>
2024-06-10 15:48:12 -0300

    jogo completo

--- Assets/Scripts/Aleatorio/LoadInteractibleLevelTemplate.cs
+++ Assets/Scripts/Aleatorio/LoadInteractibleLevelTemplate.cs
@@ -15,10 +15,6 @@ public class LoadInteractibleLevelTemplate : MonoBehaviour
         {
             SceneManager.LoadScene("Pergunta 1", LoadSceneMode.Single); 
         }
-        else if (itemToPassToNextScene == EItems.StrawCup)
-        {
-            SceneManager.LoadScene("Pergunta 2", LoadSceneMode.Single);
-        }
         else
         {
             SceneManager.LoadScene("InteractibleLevel", LoadSceneMode.Single); 
